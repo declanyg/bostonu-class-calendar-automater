cea057c6d08f4c311a960a1e9620c5b7bbb4895d
declanyg <declan@youngs.asia>
2023-09-06 22:50:14 -0400

    Added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const {google} = require('googleapis');
+const cors = require("cors");
 
 const bodyParser = require('body-parser')
 require('dotenv').config();
@@ -11,6 +12,7 @@ const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors());
 
 //Pupeteer Scraper
 
